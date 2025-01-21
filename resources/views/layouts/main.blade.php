6c3fb65e7f37d51fb9a66391fe92d3b3360fffea
leapena <leapena6@gmail.com>
2025-01-21 17:52:54 +0800

    modified main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,6 @@ class="light-style layout-menu-fixed layout-compact"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <meta name="description" content="" />
-
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon/favicon.jpg')}}" />
 
